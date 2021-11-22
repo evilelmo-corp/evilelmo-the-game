47b8ae0a749d915503ce0f6be26887ccbabc1c48
TGZ7 <togonlopez@gmail.com>
2021-11-22 22:18:09 +0100

    commit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys, pygame as pg, time, random, numpy as np
 import mapa
+
 pg.init()
 size = 1080,720
 screen = pg.display.set_mode(size)
 width,height = size
+
 #board = pg.image.load('Images\Plano-de-casa-de-3-dormitorios-2-baño-y-jardín-frontal.jpg')
 #board = pg.transform.scale(board, (1080,720))
 
@@ -13,8 +15,10 @@ clock = pg.time.Clock()
 elmo = pg.image.load('Images\evilelmo.png')
 elmo = pg.transform.scale(elmo, (50,43))
 elmorect = elmo.get_rect()
+
 def car(pos):
     screen.blit(elmo,(pos))
+
 pos = [0, 0]
 x_change_left=0
 x_change_right=0
@@ -22,12 +26,14 @@ y_change_up=0
 y_change_down=0
 sp = 0.5
 run = True
+
 def tiles(mapita):
     global tilewall
     for y, line in enumerate(mapita):
         for x, c in enumerate(line):
             if c == 1:
                 screen.blit(mapa.tilewall, (x * 32, y * 32))
+
 while run:
     screen.fill(black)
     car(pos)
